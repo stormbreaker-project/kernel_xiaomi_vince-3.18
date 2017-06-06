306cabc516f4673ce023718058dbd715da63186c
Kees Cook <keescook@chromium.org>
2017-06-05 21:52:30 -0700

    BACKPORT: arm64, vdso: Define vdso_{start,end} as array
    Adjust vdso_{start|end} to be char arrays to avoid compile-time analysis
    that flags "too large" memcmp() calls with CONFIG_FORTIFY_SOURCE.
    
    Cc: Jisheng Zhang <jszhang@marvell.com>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Suggested-by: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Kees Cook <keescook@chromium.org>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    Change-Id: I67b094d1e437c68ad75695b6b4c92f53eec5b6d0
    (cherry-picked from commit dbbb08f500d6146398b794fdc68a8e811366b451)
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>
    [krasCGQ: Update resolution for latest android-3.18 state]
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- arch/arm64/kernel/vdso.c
+++ arch/arm64/kernel/vdso.c
@@ -37,7 +37,7 @@
 #include <asm/vdso.h>
 #include <asm/vdso_datapage.h>
 
-extern char vdso_start, vdso_end;
+extern char vdso_start[], vdso_end[];
 static unsigned long vdso_pages __ro_after_init;
 
 /*
